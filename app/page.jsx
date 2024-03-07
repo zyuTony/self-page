d707b4670b88c61c8caf9357b8658290164d1f16
yzy-from-X1 <zongyuan1998@gmail.com>
2024-03-06 22:20:11 -0800

    test update

--- app/page.jsx
+++ app/page.jsx
@@ -5,6 +5,11 @@ export default function Home() {
   return (
     <>
       <p>Welcome to Tony's homepage - in progress</p>
+      <p>Just wasted couple of hours on AWS.</p>
+      <p>
+        And here we go, deployed literally within 2 minutes on vercel. what the
+        hell?
+      </p>
     </>
   );
 }
